c0c64569c23c87ae2c314bcd30cf1fc111336acf
pedroalpacheco <pedro.pacheco.a@gmail.com>
2019-04-15 09:43:47 -0300

    Modificado entrada de dados

--- read-text.js
+++ read-text.js
@@ -2,7 +2,12 @@ var fs = require('fs');
 
 try {
     var data = fs.readFileSync('arq-limpo3.txt', 'ascii');
-    console.log(data);
+        //console.log(typeof data)  
+        //console.log(data)   
+        for(i in data){
+            console.log(typeof data)
+        }
+    
 }
 catch (err) {
     console.error("There was an error opening the file:");
--- teste.js
+++ teste.js
@@ -1,7 +1,24 @@
-
 function mostra(ip){
     var geoip = require('geoip-lite');
     var geo = geoip.lookup(ip)
     console.log(geo)
 }
-mostra("35.187.180.136")
+//mostra("35.187.180.136")
+
+
+/*var fs = require('fs');
+
+try {
+    var data = fs.readFileSync('arq-limpo3.txt', 'ascii');
+        for(let i in data){
+            //console.log(data);
+            mostra(data)
+            
+        }
+         
+}
+catch (err) {
+    console.error("There was an error opening the file:");
+    console.log(err);
+}*/
+
