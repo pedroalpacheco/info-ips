fa037c1e9944d982fc47ceea1d1c7b5097dcb1b0
pedroalpacheco <pedro.pacheco.a@gmail.com>
2019-04-15 16:17:06 -0300

    Tentativas

--- read-text.js
+++ read-text.js
@@ -1,15 +1,41 @@
+function mostra(ip){
+    var geoip = require('geoip-lite');
+    var geo = geoip.lookup(ip)
+    //console.log(geo)
+    return geo
+}
+
 var fs = require('fs');
 
-try {
+let data = fs.readFileSync('arq-limpo3.txt', 'ascii');
+//console.log(data)
+let datarray = data.split('\n')
+//console.log(datarray)
+let allips = datarray.map(function(num){
+    //console.log("este ip: " + num)
+    mostra(num)
+})
+
+/*try {
     var data = fs.readFileSync('arq-limpo3.txt', 'ascii');
         //console.log(typeof data)  
-        //console.log(data)   
-        for(i in data){
-            console.log(typeof data)
+        {
+            let datarray = data.split('\n')
+            let mostra = datarray.map(function(num){
+                console.log(num)
+            });
+            
+            //var geoip = require('geoip-lite');
+            //var geo = geoip.lookup(datarray)
+            //console.log(datarray[15]) 
+            //for(i in datarray){
+             //   console.log(i[1])
+            //}  
+            
         }
-    
-}
+
 catch (err) {
     console.error("There was an error opening the file:");
     console.log(err);
-}
+}*/
+
