3e79a72e5ea8c635b4c6849736e36c27a689681b
pedroalpacheco <pedro.pacheco.a@gmail.com>
2019-04-16 09:46:28 -0300

    Organização e tarefas

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # info-ips
 Simple program for ips information
+
+
+#Tarefas:
+
+()-Indentificar ips;
+()-Identificar pais de ips;
+()-Relatório por pais, somatório de acesso por país.
+
--- read-text.js
+++ read-text.js
@@ -1,8 +1,11 @@
 function mostra(ip){
     let geoip = require('geoip-lite');
     let geo = geoip.lookup(ip)
-    //console.log(ip)
     console.log(geo)
+    /*Acessando objeto país*/
+    //console.log(geo.country)
+    /*coordenadas GEO latitude/longitude*/
+    //console.log(geo.ll)
     //return geo
 }
 
@@ -10,6 +13,7 @@ var fs = require('fs');
 
 let data = fs.readFileSync('arq-limpo3.txt', 'ascii');
 //console.log(data)
+/*Da split em dados*/
 let datarray = data.split('\n')
 //console.log(datarray)
 let allips = datarray.map(function(num){
